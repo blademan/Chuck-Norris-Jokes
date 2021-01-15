622cb6c9420b925ee5b6225f99ead90fcb58dc1f
Eduards Leimanis <eduards.leimanis@gmail.com>
2021-01-15 20:01:17 +0200

    v 1.0

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
+const blockquote = document.querySelector('.blockquote');
+const btn = document.querySelector('.btn');
+const img = document.querySelector('img');
 
+btn.addEventListener('click', getJokes);
 
+getJokes();
+
+async function getJokes() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+
+  const results = await fetch(
+    'https://api.chucknorris.io/jokes/random',
+    config
+  );
+  const data = await results.json();
+  console.log(data);
+  blockquote.innerHTML = data.value;
+  img.src = data.icon_url;
+}
